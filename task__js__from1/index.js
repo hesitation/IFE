df8a5b8f10e342fdd3c846bd1b2a689c13cbdf4a
Tracy <15008246873@163.com>
2017-03-20 16:17:16 +0800

    表单1第二版，逻辑封装在函数中

--- task__js__from1/index.js
+++ task__js__from1/index.js
@@ -1,38 +1,60 @@
-function testInput () {
-	var input = document.getElementById('username').value;
- 	var re1 = /\w+/g;  // 匹配字符
- 	var re2 = /[\u4e00-\u9fa5]/g;  // 匹配中文
- 	var len = 0;  // 字符个数
- 	
- 	if(input == '') {
- 		len = 0;
- 	} else {
- 		var str = input.match(re1).join('');
- 		var chinese = input.match(re2);
- 		if(chinese != null) {
- 			chinese = chinese.join('');
-	 		len = str.length + chinese.length * 2;
- 		} else {
- 			len = str.length;
- 		}
- 	}
- 	console.log(len);
+window.onload = function() {
+	var btn = document.getElementById('btn');
 
- 	if(len == 0) {
- 		document.getElementsByTagName('input')[0].style.borderColor = 'red';
- 		document.getElementsByTagName('p')[0].style.color = 'red';
- 		document.getElementsByTagName('p')[0].innerHTML = '名称不能为空';
- 	}
- 	else if(len >=4 && len <= 16) {
- 		document.getElementsByTagName('input')[0].style.borderColor = 'green';
- 		document.getElementsByTagName('p')[0].style.color = 'green';
- 		document.getElementsByTagName('p')[0].innerHTML = '名称格式正确';
- 	} else {
- 		document.getElementsByTagName('input')[0].style.borderColor = 'red';
- 		document.getElementsByTagName('p')[0].style.color = 'red';
- 		document.getElementsByTagName('p')[0].innerHTML = '必填，长度为4~16个字符';
- 	} 
+	btn.addEventListener('click', function() {
+		var inputMsg = document.getElementById('msg');
+		var tipDom = document.getElementById('tip');
+
+		var data = validate(inputMsg.value);
+		console.log(data);
+		inputMsg.style.borderColor = data.color;
+		tipDom.innerText = data.message;
+		tipDom.style.color = data.color;
+	}, false);
+}
+
+function validate (str) {
+	let len = checkLength(str);
+	console.log(len);
+	let data = {
+		message: "",
+		color: ''
+	}
+
+	if(len === 0) {
+		data.message = "名称不能为空";
+		data.color = 'red';
+		return data;
+	} else if(len >= 4 && len <= 16) {
+		data.message = "名称格式正确";
+		data.color = 'green';
+		return data;
+	} else {
+		data.message = "必填，长度为4~16个字符";
+		data.color = 'red';
+		return data;
+	}
 
 }
 
-document.getElementsByTagName('button')[0].addEventListener('click', testInput, false);	
+function checkLength(str) {
+	if(str === '') {
+		return 0;
+	}
+	var re_char = /\w+/g;
+	var re_ch = /[\u4e00-\u9fa5]/g;
+	var char = str.match(re_char);  // 数字、字母、下划线
+	var ch = str.match(re_ch);  // 中文字符
+
+	var char_len = 0;
+	var ch_len = 0;
+
+	if(char != null) {
+		char_len = char.join('').length;
+	} 
+	if(ch != null) {
+		ch_len = ch.join('').length * 2;
+	}
+	
+	return char_len + ch_len;
+}
